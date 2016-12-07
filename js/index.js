8d9532b103047ba4c7b37fa9f8ab8b2610462604
Christopher Metz <cmetz7@gmail.com>
2016-12-06 16:02:55 -0800

    Close #8 Built surf page with JS

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 (function() {
   'use strict';
+
   $('.button-collapse').sideNav();
+
+  const option = 'surf';
+
+  if (option === 'surf') {
+    const $body = $('body');
+    const $h1 = $('h1');
+    $body.prop('style', 'background-image: url(https://static.pexels.com/photos/32318/pexels-photo.jpg)');
+    $h1.text('Go Surfing at Twin Rivers Dude!');
+  }
 })();
